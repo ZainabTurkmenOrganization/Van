4922685c7472677384ca99e5b0428c33b8a05714
Zaianb <132147692+sampleRID@users.noreply.github.com>
2023-11-12 18:46:41 +0430

    commit

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 export default function Home() {
     return (
         <div className="home-container">
-            <h1>You got the travel plans, we got the travel vans. So let's go to the travel around the world and, see amazing things with our families and friends.</h1>
+            <h1>You got the travel plans, we got the travel vans. So let's go to the travel and, see the world's amazing things with our families.</h1>
             <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip. So let's joing us.</p>
             <Link to="vans">Find your van</Link>
         </div>
