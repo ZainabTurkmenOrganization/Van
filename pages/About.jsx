cbaf9fc9e14ceae41e74407547415fc658e2e667
Zaianb <132147692+sampleRID@users.noreply.github.com>
2023-11-14 11:56:48 +0430

    COMMIT

--- pages/About.jsx
+++ pages/About.jsx
@@ -11,9 +11,19 @@ export default function About() {
                 <p>Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch. (Hitch costs extra 😉)</p>
                 <p>Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.</p>
             </div>
-            <div className="about-page-cta">
-                <h2>Your destination is waiting.<br />Your van is ready.</h2>
-                <Link className="link-button" to="/vans">Explore our vans</Link>
+            <div className="ABOUT-PAGE">
+                <div className="about-page-cta">
+                    <h2>Your destination is waiting.<br />Your van is ready.</h2>
+                    <Link className="link-button" to="/vans">Explore our vans</Link>
+                </div>
+                <div className="about-page-cta">
+                    <h2>Your destination is waiting.<br />Your van is ready.</h2>
+                    <Link className="link-button" to="/vans">Explore our vans</Link>
+                </div>
+                <div className="about-page-cta">
+                    <h2>Your destination is waiting.<br />Your van is ready.</h2>
+                    <Link className="link-button" to="/vans">Explore our vans</Link>
+                </div>
             </div>
         </div>
     );
